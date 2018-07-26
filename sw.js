dc4d46de77627ea7f1337f33e1d3738d15938462
Ivan <ivan.silva_123@hotmail.com>
2018-07-26 22:37:43 +0100

    Aria label added | SW Fetch corrected

--- sw.js
+++ sw.js
@@ -46,9 +46,28 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
-		caches.match(event.request)
+	  caches.match(event.request)
 		.then(function(response) {
-			return response || fetch(event.request);
-		})
+		  if (response) {
+			return response;
+		  }
+		  var fetchRequest = event.request.clone();
+  
+		  return fetch(fetchRequest).then(
+			function(response) {
+			if(!response || response.status !== 200 || response.type !== 'basic') {
+			return response;
+			}
+			var responseToCache = response.clone();
+
+			caches.open(CACHE_NAME)
+			.then(function(cache) {
+				cache.put(event.request, responseToCache);
+			});
+
+			return response;
+		}
+		);
+	})
 	);
 });
